79901b85db5fb69e5ecdf35d6de6eeb637ae62d8
Maclain Wiltzer <mwiltzer@gmail.com>
2022-06-06 11:41:12 -0400

    Updated assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MethodInvoker")]
+[assembly: AssemblyTitle("SemaphoreSlim Throttling")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MethodInvoker")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("SemaphoreSlim Throttling")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
